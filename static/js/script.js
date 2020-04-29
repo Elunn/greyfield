c4a49f4ee28bd605ed2aec2d97d0e295363c5b3e
Elunn <31861423+Elunn@users.noreply.github.com>
2020-04-29 22:00:17 +0100

    first

---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+window.onload = function () {
+  var video = document.getElementById('video'),
+      seeker = document.getElementById('seeker'),
+      volume = document.getElementById('volume'),
+      play = document.getElementById('playPause'),
+      mute = document.getElementById('mute'),
+      full = document.getElementById('fullScreen');
+
+  seeker.addEventListener('change', function() {
+    var time = video.duration * (seeker.value / 100);
+    video.currentTime = time;
+	  play.className = 'pause';
+  });
+
+  video.addEventListener('timeupdate', function() {
+    var value = (100 / video.duration) * video.currentTime;
+    seeker.value = value;
+  });
+
+  video.addEventListener('ended', function() {
+    play.className = 'pause';
+  });
+
+  seeker.addEventListener('mousedown', function() {
+    video.pause();
+  });
+  seeker.addEventListener('mouseup', function() {
+    video.play();
+  });
+
+  volume.addEventListener('change', function() {
+    video.volume = volume.value;
+  });
+
+  play.addEventListener('click', function () {
+    if (video.paused == true) {
+      video.play();
+      play.className = 'pause';
+    }
+    else {
+      video.pause();
+      play.className = 'play';
+    }
+  });
+
+  mute.addEventListener('click', function () {
+    if (video.muted == false) {
+      video.muted = true;
+      mute.className = 'off';
+    }
+    else {
+      video.muted = false;
+      mute.className = 'on';
+    }
+  });
+
+  full.addEventListener('click', function() {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    }
+    else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen();
+    }
+    else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen();
+    }
+  });
+}
